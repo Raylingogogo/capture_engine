21b73a7af3f553e7511eab1e96892a08558fc59c
joew <Joe_Wang@quantatw.com>
2017-09-28 19:48:14 +0800

    Increase version to 20170929 for release

--- C++/winmain.cpp
+++ C++/winmain.cpp
@@ -20,7 +20,7 @@
 int g_threshold, g_op_mode, g_countToCapture, g_device_type, g_select_no, g_resolutionIndex, g_pin_no;
 int g_sum=0, g_validNum=0;
 
-WCHAR *g_toolVersion = L"version: 20170810";
+WCHAR *g_toolVersion = L"version: 20170929";
 FILE *file_log;
 HWND initWindow;
 
